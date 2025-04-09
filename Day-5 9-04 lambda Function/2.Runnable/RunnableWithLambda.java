618c467d3e5d480bddb8b4ee643c57871163558d
Shreyas Salunke <110183538+salunke12@users.noreply.github.com>
2025-04-10 02:37:11 +0530

    Delete Day-5 9-04 lambda Function directory

--- Day-5 9-04 lambda Function/2.Runnable/RunnableWithLambda.java
+++
@@ -1,9 +0,0 @@
-public class RunnableWithLambda{
-    public static void main(String[] args) {
-        Runnable runnable = ()-> {
-            System.out.println("Thread is running using Lambda!");
-        };
-        Thread thread= new Thread(runnable);
-        thread.start();
-    }
-}
